69865ecce40661daa7fc3bd9db8fade198f1e12f
pigeontech <pigeontech@gmail.com>
2013-12-30 15:41:17 -0500

    Changed default page and mysql password

---
+++ www/default/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Default</title>
+</head>
+<body>
+	
+<h1>It Works!</h1>
+<p>
+	The installation was successful!
+</p>
+<h3>Useful Links</h3>
+<ul>
+	<li><a href="phpinfo.php">PHP Info</a> - View information about the software on this server.</li>
+	<li><a href="phpmyadmin">phpMyAdmin</a> - Create and manage databases.</li>
+</ul>
+
+</body>
+</html>
