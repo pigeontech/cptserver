48f03bc99b51a12b13663436afe8dba34ad18cab
Scott Pigeon <pigeontech@gmail.com>
2015-03-27 06:59:57 -0400

    Added ~/.composer/vendor/bin to path

--- config/puppet/modules/composer/manifests/init.pp
+++ config/puppet/modules/composer/manifests/init.pp
@@ -18,4 +18,13 @@ class composer ($startmsg)
 		command => "curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer",
 		require => Notify['msg_composer']
 	}
+
+	# Add composer/vender/bin to path
+	file
+	{
+		"/etc/profile.d/composer.sh":
+		mode => 0644,
+		content => 'export PATH="~/.composer/vendor/bin:$PATH"',
+		require => Exec['composer']
+	}
 }
